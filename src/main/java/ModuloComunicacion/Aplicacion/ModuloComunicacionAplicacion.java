545c1b2192a91259d4884a0318b0de1b0464fdb2
Nico <62857840+NicoSTeam@users.noreply.github.com>
2024-05-06 16:01:15 -0300

    interface comunicacionApli

--- src/main/java/ModuloComunicacion/Aplicacion/ModuloComunicacionAplicacion.java
+++ src/main/java/ModuloComunicacion/Aplicacion/ModuloComunicacionAplicacion.java
@@ -1,7 +1,11 @@
 package ModuloComunicacion.Aplicacion;
 
 public interface ModuloComunicacionAplicacion {
-	
+	//public void notificarSalgoInsuficiente(Cliente);
+	//public void notificarTarjetaBloqueada(Cliente);
+	//public void notificarInformacion(texto);
+	//public Cliente altaCliente(Cliente);
+	//public Cliente ObtenerNotiPorCliente(Cliente):set(Notificaciones);
 	
 
 }
