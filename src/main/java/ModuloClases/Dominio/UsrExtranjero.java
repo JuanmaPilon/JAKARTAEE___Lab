9d350ed2d9ecab7d858354f18ee5adfa63670346
Juanma <juanmartin.pilon@gmail.com>
2024-05-10 20:52:21 -0300

    Pusheando otra vez mas cosas..., mismo de todo lo que he trabajodo hoy..

--- src/main/java/ModuloClases/Dominio/UsrExtranjero.java
+++ src/main/java/ModuloClases/Dominio/UsrExtranjero.java
@@ -6,6 +6,9 @@ public class UsrExtranjero extends Usuario {
         super(); 
     }
 
+
+    //tengo que armar esta tambien, cuando pueda sigo
+
     public UsrExtranjero(String ci, String nombreUsuario, String email) {
         super(ci, nombreUsuario, email); 
     }
--- src/main/java/ModuloClases/Dominio/UsrNacional.java
+++ src/main/java/ModuloClases/Dominio/UsrNacional.java
@@ -1,12 +1,19 @@
 package ModuloClases.Dominio;
 
+import ModuloGestionClientes.Dominio.ClienteSucive;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class UsrNacional extends Usuario {
 
-    public UsrNacional() {
-        super(); 
+    private ClienteSucive clienteSucive;
+
+    public UsrNacional() {};
+
+    public UsrNaciona(Long ci) {
+       // this.ci = ci; // no me lo toma ni con public ni con protected, capaz es algo de lombok
     }
 
-    public UsrNacional(String ci, String nombreUsuario, String email) {
-        super(ci, nombreUsuario, email); 
     }
-}
