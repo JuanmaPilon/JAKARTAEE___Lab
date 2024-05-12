26d0b9d05c1c884f8b500389149c2c18cea34928
Carlos Santana <carlossantana5017@gmail.com>
2024-05-12 10:20:05 -0300

    ModuloMediosPago
    primera implementacion de los tres metodos de consultaDePagos

--- src/main/java/ModuloMediosPago/Aplicacion/ModuloMediosPagoAplicacion.java
+++ src/main/java/ModuloMediosPago/Aplicacion/ModuloMediosPagoAplicacion.java
@@ -10,5 +10,5 @@ public interface ModuloMediosPagoAplicacion {
 	public void notificarPago(ClienteTelepeaje cliente, Nacional vehiculo, double importe, Tarjeta tarjeta);
     public List<Pagos> consultaDePagos(Date fechaInicial, Date fechaFinal);
 	public List<Pagos> consultaDePagos(ClienteTelepeaje cliente);
-	public List<Pagos> consultaDePagos(ClienteTelepeaje cliente, Vehiculo vehiculo);
+	public List<Pagos> consultaDePagos(ClienteTelepeaje cliente, Nacional vehiculo);
 }
--- src/main/java/ModuloMediosPago/Aplicacion/ModuloMediosPagoImpl.java
+++ src/main/java/ModuloMediosPago/Aplicacion/ModuloMediosPagoImpl.java
@@ -6,8 +6,7 @@ import ModuloMediosPago.Dominio.Repo.RepoMediosPagoImpl;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @ApplicationScoped
 public class ModuloMediosPagoImpl implements ModuloMediosPagoAplicacion {
@@ -20,7 +19,7 @@ public class ModuloMediosPagoImpl implements ModuloMediosPagoAplicacion {
         this.repoMediosPago = new RepoMediosPagoImpl();
         this.moduloComunicacion = new ModuloComunicacion();
     }
-
+    
     @Override
     public void altaCliente(ClienteTelepeaje cliente, Tarjeta tarjeta) {
         cliente.getCuentaPOSTPaga().setTarjeta(tarjeta); //le asocio la tarjeta a la cuenta postpaga del cliente
@@ -32,7 +31,7 @@ public class ModuloMediosPagoImpl implements ModuloMediosPagoAplicacion {
         sino tambien en el repo del modulo de comunicacion */
         ClienteTelepeaje clienteModulo = repoMediosPago.buscarCliente(cliente.getCi()); // busco el cliente que esta ingresado en el repo mediante el que se me provee
         String texto = "Nuevo pago por el importe de :" + importe + " Matricula: "+ vehiculo.getMatricula().getNroMatricula() +" Tarjeta: "+ tarjeta.getNombre(); //creo la notificacion
-        moduloComunicacion.notificarInformacion(cliente.getCi(),texto);// le derivo la tarea de la notificacion al modulo correspondiente
+        moduloComunicacion.notificarInformacion(cliente.getCi(),texto);// le derivo la tarea de la notificacion al modulo correspondiente (ver como hacer con eventos)
     }
 
     @Override
@@ -40,21 +39,37 @@ public class ModuloMediosPagoImpl implements ModuloMediosPagoAplicacion {
         //1- Recorro lista de clientes del repo
         //2- obtengo todos los pagos de cada cliente que esten entre las fechas dadas y los voy guardando en orden
         //3- retorno la lista de pagos
-        return List.of();
-    }
-    @Override
-    public List<Pagos> consultaDePagos(ClienteTelepeaje cliente){
-        //1- busco al cliente en el repo
-        //2- obtengo su lista de pagos y la retorno
-        return List.of();
+        List<Pagos> pagosEntreFechas = new ArrayList<>();//creo la nueva lista
+        for (ClienteTelepeaje cliente : repoMediosPago.obtenerTodosLosClientes()) { //recorro lista clientes
+            for (Pagos pago : cliente.getPagos()) {  //recorro pagos de cada cliente
+                if(pago.getFecha().after(fechaInicial) && pago.getFecha().before(fechaFinal)) { //me aseguro que el pago corresponda a las fechas
+                    pagosEntreFechas.add(pago);  //lo agrego a la nueva lista
+                }
+            }
+        }
+        pagosEntreFechas.sort((pago1, pago2) -> pago1.getFecha().compareTo(pago2.getFecha())); // supuestamente ordena la lista de pagos por fechas
+        return pagosEntreFechas; //retorno los pagos
     }
 
     @Override
-    public List<Pagos> consultaDePagos(ClienteTelepeaje cliente, Vehiculo vehiculo) {
+    public List<Pagos> consultaDePagos(ClienteTelepeaje cliente){ //todo bien
+        //1- busco al cliente en el repo
+        //2- obtengo su lista de pagos y la retorno
+        return repoMediosPago.buscarCliente(cliente.getCi()).getPagos();
+    }
+
+    @Override
+    public List<Pagos> consultaDePagos(ClienteTelepeaje cliente, Nacional vehiculo) {
         //1- busco al cliente en el repo
         //2- obtengo su lista de pagos, la recorro y me quedo solo con los pagos que se correspondan a la matricula del vehiculo por parametro
         //3- retorno nueva lista
-        return List.of();
+        List<Pagos> pagosVehiculoConcreto = new ArrayList<>();
+        for (Pagos pago : repoMediosPago.buscarCliente(cliente.getCi()).getPagos()) {
+            if (Objects.equals(pago.getMatricula(), vehiculo.getMatricula().getNroMatricula())){
+                pagosVehiculoConcreto.add(pago);
+            }
+        }
+        return pagosVehiculoConcreto;  //si no hay pagos con esa matricula esto se retorna vacio
     }
 
 }
