49a39d0247370352fcd14c64f001cead70420587
Juanma <juanmartin.pilon@gmail.com>
2024-05-29 22:35:09 -0300

    Arreglado lo de los poms, estan referenciados y conectados crrectamente, falta ver porque no esta aceptando todo lo de DTOs al importar. Si saben rezar, ya saben

--- Sistema-Trafico/API Medios de Pagos (RESTFul)/src/main/java/ApiClient/Aplicacion/ApiPago.java
+++ Sistema-Trafico/API Medios de Pagos (RESTFul)/src/main/java/ApiClient/Aplicacion/ApiPago.java
@@ -1,35 +1,30 @@
 package ApiClient.Aplicacion;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import ApiClient.Aplicacion.IApiPago;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.tallerjava.*;
-
 
 @ApplicationScoped
 @Path("/mensaje")
 public class ApiPago {
 
     @GET
+    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Response mensaje() {
-        System.out.println("Todo ok negro cagado");
+        System.out.println("Todo ok negro");
         return Response.ok("Tdo ok").status(200, "OK").build();
     }
 
     @POST
+    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public Response createUsuarioDTO(UsuarioDTO usuarioDTO) {
-        System.out.println("Todo ok negro cagado");
-        usuarioDTO = new UsuarioDTO();
+    public Response createUsuarioDTO() {
+        System.out.println("Todo ok negro");
+//        usuarioDTO = new UsuarioDTO();
 
         return Response.ok("Funciona ok").status(200, "Nice").build();
     }
