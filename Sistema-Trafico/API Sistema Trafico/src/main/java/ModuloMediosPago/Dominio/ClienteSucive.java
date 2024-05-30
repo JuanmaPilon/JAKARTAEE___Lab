f4a394d0ce26df4559cd089e39c077d64edda766
Nico <nicosilva19@hotmail.com>
2024-05-30 08:32:21 -0300

    Problemas con JPA

--- Sistema-Trafico/API Sistema Trafico/src/main/java/ModuloMediosPago/Dominio/ClienteSucive.java
+++ Sistema-Trafico/API Sistema Trafico/src/main/java/ModuloMediosPago/Dominio/ClienteSucive.java
@@ -1,5 +1,24 @@
 package ModuloMediosPago.Dominio;
 
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Table(name = "mediosPago_cliente_sucive")
 public class ClienteSucive {
+//    @Id
+//    private String ci;
+//
+//    private String nombre;
+//
+//    private int saldo;
 
 }
