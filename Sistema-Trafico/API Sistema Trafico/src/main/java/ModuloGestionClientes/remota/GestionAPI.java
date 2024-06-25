b96a434a6b4115f7d7650c647253c1980c3c3ce9
Carlos Santana <carlossantana5017@gmail.com>
2024-06-24 21:03:58 -0300

    endpoint para consultar las pasadas por peaje :car:

--- Sistema-Trafico/API Sistema Trafico/src/main/java/ModuloGestionClientes/remota/GestionAPI.java
+++ Sistema-Trafico/API Sistema Trafico/src/main/java/ModuloGestionClientes/remota/GestionAPI.java
@@ -1,15 +1,23 @@
 package ModuloGestionClientes.remota;
 import ModuloGestionClientes.Aplicacion.ModuloGestionClientes;
+import ModuloGestionClientes.Dominio.ClienteTelepeaje;
+import ModuloGestionClientes.Dominio.PasadaPorPeaje;
+import ModuloGestionClientes.Dominio.Repo.RepoClientes;
+import ModuloGestionClientes.Dominio.Vehiculo;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.jboss.logging.Logger;
 import ModuloPeaje.Aplicacion.ModuloPeajeImpl;
+import org.tallerjava.DatosConsultaPasadaDTO;
 import org.tallerjava.DatosPagoDTO;
 
+import java.util.Set;
 
 
 @ApplicationScoped
@@ -19,6 +27,8 @@ public class GestionAPI {
 
     @Inject
     private ModuloGestionClientes moduloGestion;
+    @Inject
+    private RepoClientes repo;
 
     @GET
     @Path("/PostPago")
@@ -33,5 +43,17 @@ public class GestionAPI {
     public void prePago(DatosPagoDTO idDTO) {
         moduloGestion.realizarPrePago(idDTO.getTag(), idDTO.getImporte());
         log.infof("Notificando PrePago " + idDTO);
+    }
+    @GET
+    @Path("/ObtenerPasadas")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<PasadaPorPeaje> ObtenerPasadas(DatosConsultaPasadaDTO dto) {
+        ClienteTelepeaje clienteTelepeaje = repo.buscarClienteTelePorCI(dto.getCedula());
+        Vehiculo vehiculo = repo.BuscarTag(dto.getTag());
+        Set<PasadaPorPeaje> set = moduloGestion.consultarPasadas(clienteTelepeaje,dto.getFechaInicio(),dto.getFechaFin(),vehiculo);
+        log.infof("Consultando Pasadas");
+        //return Response.ok("Conto un vehiculo").status(200,"OK").build();
+        return set;
     }
 }
